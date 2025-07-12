6a7346d05752e6b779c70f773a105d2695da1807
Amira Abdurahman <amiraabdurahman8@gmail.com>
2025-07-12 18:46:44 +0300

    necessary files added

--- 0x04-TypeScript/task_0/.eslintrc.js
+++ 0x04-TypeScript/task_0/.eslintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  parser: "@typescript-eslint/parser",
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    "plugin:@typescript-eslint/recommended"
   ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module"
+  },
+  rules: {}
 };
