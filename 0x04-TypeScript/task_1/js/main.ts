764632289a0a0ca4857b9fb7adde7d0f1f0f36f1
Amira Abdurahman <amiraabdurahman8@gmail.com>
2025-07-12 18:44:36 +0300

    main.ts added

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,20 @@
+// Define the interface
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // Index signature to allow extra properties
+}
+
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false, 
+};
+
+console.log(teacher3);
